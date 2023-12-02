9417f2bb0e128eb0ad071fb66fd282b96e7434ed
isu-nice <isu010614@naver.com>
2023-12-03 00:30:37 +0900

    feat: 테스트용 회원 추가

--- src/main/java/hello/login/TestDataInit.java
+++ src/main/java/hello/login/TestDataInit.java
@@ -24,5 +24,11 @@ public class TestDataInit {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
 
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+
+        memberRepository.save(member);
     }
 }
